3ff088d94686423d8ac9dcb17273cf7a58b1825a
= <=>
2023-04-03 18:51:36 +0300

    BFS ve DFS gösterimden önce çalışsın

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from graph import AlgorithmHelper
 import networkx as nx
 import matplotlib.pyplot as plt
 g = AlgorithmHelper('matrix.txt')
+
+print(f"bfs result: {g.bfs(0, 1)}")
+print(f"dfs result: {g.dfs(0, 1)}")
+
 pos = nx.spring_layout(g.graph)
 nx.draw_networkx(g.graph, pos)
 labels = nx.get_edge_attributes(g.graph, 'weigth')
 nx.draw_networkx_edge_labels(g.graph, pos, edge_labels=labels)
 plt.show()
 
-print(f"bfs result: {g.bfs(0, 2)}")
-print(f"dfs result: {g.dfs(0, 2)}")
