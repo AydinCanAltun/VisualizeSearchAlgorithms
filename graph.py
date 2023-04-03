d07fa08357f96791c11f05f63d69d5371e466ba2
= <=>
2023-04-03 23:10:12 +0300

    UCS Düzenlendi, Dls ve Bidirectional Search Eklendi Co-authored-by: Osman Şimşek <osmansimsek@users.noreply.github.com>

--- graph.py
+++ graph.py
@@ -1,4 +1,4 @@
-from queue import Queue
+from queue import Queue, PriorityQueue
 import heapq
 import networkx as nx
 
@@ -74,17 +74,74 @@ class AlgorithmHelper:
       path.pop()
       return None    
 
-    def ucs(graph, start, goal):
-      visited = set()
-      queue = [(0, start, [])]
-      while queue:
-          (cost, node, path) = heapq.heappop(queue)
-          if node not in visited:
-              visited.add(node)
-              path = path + [node]
-              if node == goal:
-                  return (cost, path)
-              for neighbor, neighbor_cost in graph[node].items():
-                  if neighbor not in visited:
-                      heapq.heappush(queue, (cost + neighbor_cost, neighbor, path))
-      return None
+    def ucs(self, start, goal):
+        explored = set()
+        pq = PriorityQueue()
+        pq.put((0, start, [start]))
+
+        while not pq.empty():
+            cost, node, path = pq.get()
+            if node == goal:
+                return path
+            if node not in explored:
+                explored.add(node)
+                for neighbor, weight in self.m_adj_list[node]:
+                    if neighbor not in explored:
+                        pq.put((cost + weight, neighbor, path + [neighbor]))
+
+        return None
+    
+
+    def dls(self, deep_limit, start, target, path = [], visited = set()):
+        path.append(start)
+        visited.add(start)
+        if start == target:
+            return path
+        elif deep_limit == 0:
+            path.pop()
+            return None
+        else:
+            for (neighbour, weight) in self.m_adj_list[start]:
+                if neighbour not in visited:
+                    result = self.dls(deep_limit - 1, neighbour, target, path, visited)
+                    if result is not None:
+                        return result
+            path.pop()
+        return None
+    
+    def bidirectional_search(self, start, goal):
+        # create two sets to track visited nodes
+        visited_forward = set()
+        visited_backward = set()
+        # create two queues for BFS
+        queue_forward = [start]
+        queue_backward = [goal]
+
+        forwardPath = self.bfs(start, goal)
+        if goal in forwardPath:
+            return forwardPath
+        
+        backwardPath = self.bfs(goal, start)
+        if start in backwardPath:
+            return backwardPath
+       
+        # while queue_forward and queue_backward:
+        #     # perform BFS from the forward direction
+        #     current_forward = queue_forward.pop(0)
+        #     visited_forward.add(current_forward)
+        #     for (neighbor, weight) in self.m_adj_list[current_forward]:
+        #         if neighbor not in visited_forward:
+        #             queue_forward.append(neighbor)
+        #     # check if the current node has been visited in the backward search
+        #     if current_forward in visited_backward:
+        #         return True
+        #     # perform BFS from the backward direction
+        #     current_backward = queue_backward.pop(0)
+        #     visited_backward.add(current_backward)
+        #     for neighbor in graph[current_backward]:
+        #         if neighbor not in visited_backward:
+        #             queue_backward.append(neighbor)
+        #     # check if the current node has been visited in the forward search
+        #     if current_backward in visited_forward:
+        #         return True
+        return None
--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 g = AlgorithmHelper('matrix.txt')
 print(f"bfs result: {g.bfs(0, 1)}")
 print(f"dfs result: {g.dfs(0, 1)}")
+print(f"ucs result: {g.ucs(0, 1)}")
+print(f"dls result: {g.dls(3, 0, 1, [], set())}")
+print(f"bidirectional reuslt: {g.bidirectional_search(0, 1)}")
 pos=nx.spring_layout(g.graph,seed=5)
 nx.draw_networkx_nodes(g.graph, pos)
 nx.draw_networkx_labels(g.graph, pos)
@@ -22,4 +25,3 @@ nx.draw_networkx_edges(g.graph, pos, edgelist=curved_edges, edge_color=curved_ed
 edge_weights = nx.get_edge_attributes(g.graph,'weight')
 nx.draw_networkx_edge_labels(g.graph, pos, edge_labels=edge_weights,rotate=False)
 plt.show()
-
