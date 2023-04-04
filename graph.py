8dac88818112459b557853d54361cc2fbfc85775
= <=>
2023-04-04 19:36:12 +0300

    Adım adım görselleştirme örnek

--- graph.py
+++ graph.py
@@ -25,41 +25,43 @@ class AlgorithmHelper:
         print("node", key, ": ", self.m_adj_list[key])
 
     def bfs(self, start_node, target_node):
-      # Set of visited nodes to prevent loops
-      visited = set()
-      queue = Queue()
+        # Set of visited nodes to prevent loops
+        visited = set()
+        queue = Queue()
 
-      # Add the start_node to the queue and visited list
-      queue.put(start_node)
-      visited.add(start_node)
+        # Add the start_node to the queue and visited list
+        queue.put(start_node)
+        visited.add(start_node)
     
-      # start_node has not parents
-      parent = dict()
-      parent[start_node] = None
+        # start_node has not parents
+        parent = dict()
+        parent[start_node] = None
 
-      # Perform step 3
-      path_found = False
-      while not queue.empty():
-          current_node = queue.get()
-          if current_node == target_node:
-              path_found = True
-              break
+        # Perform step 3
+        path_found = False
+        while not queue.empty():
+            current_node = queue.get()
+            if current_node == target_node:
+                path_found = True
+                break
   
-          for (next_node, weight) in self.m_adj_list[current_node]:
-              if next_node not in visited:
-                  queue.put(next_node)
-                  parent[next_node] = current_node
-                  visited.add(next_node)
+            for (next_node, weight) in self.m_adj_list[current_node]:
+                if next_node not in visited:
+                    queue.put(next_node)
+                    parent[next_node] = current_node
+                    visited.add(next_node)
                 
-      # Path reconstruction
-      path = []
-      if path_found:
-          path.append(target_node)
-          while parent[target_node] is not None:
-              path.append(parent[target_node]) 
-              target_node = parent[target_node]
-          path.reverse()
-      return path 
+        # Path reconstruction
+        path = []
+        if path_found:
+            path.append(target_node)
+            while parent[target_node] is not None:
+                path.append(parent[target_node]) 
+                target_node = parent[target_node]
+            path.reverse()
+            print(f"BFS Parent : {parent}")
+            print(f"BFS Visited : {visited}")
+        return path 
 
     def dfs(self, start, target, path = [], visited = set()):
       path.append(start)
--- main.py
+++ main.py
@@ -1,32 +1,36 @@
 from graph import AlgorithmHelper
 import networkx as nx
 import matplotlib.pyplot as plt
+import time
 
 g = AlgorithmHelper('matrix.txt')
-print(f"bfs result: {g.bfs(0, 1)}")
+print(f"bfs result: {g.bfs(3, 5)}")
 print(f"dfs result: {g.dfs(0, 1)}")
 print(f"ucs result: {g.ucs(0, 1)}")
 print(f"dls result: {g.dls(3, 0, 1, [], set())}")
 print(f"bidirectional reuslt: {g.bidirectional_search(0, 1)}")
-pos=nx.spring_layout(g.graph,seed=5)
-nx.draw_networkx_nodes(g.graph, pos)
-nx.draw_networkx_labels(g.graph, pos)
+i=0
+while i in range(5):
+    plt.clf()
+    pos=nx.spring_layout(g.graph,seed=5)
+    nx.draw_networkx_nodes(g.graph, pos)
+    nx.draw_networkx_labels(g.graph, pos)
 
 
-curved_edges = [edge for edge in g.graph.edges() if reversed(edge) in g.graph.edges()]
-curved_edge_colors = ['red' if e[1] == 3 else 'black' for e in curved_edges]
+    curved_edges = [edge for edge in g.graph.edges() if reversed(edge) in g.graph.edges()]
+    curved_edge_colors = ['red' if e[1] == i else 'black' for e in curved_edges]
+    print(f"curver_edge_colors: {curved_edge_colors}")
 
-straight_edges = list(set(g.graph.edges()) - set(curved_edges))
-straight_edge_colors = ['red' if e[1] == 3 else 'black' for e in straight_edges]
+    straight_edges = list(set(g.graph.edges()) - set(curved_edges))
+    straight_edge_colors = ['red' if e[1] == i else 'black' for e in straight_edges]
 
-nx.draw_networkx_edges(g.graph, pos, edge_color=straight_edge_colors, edgelist=straight_edges)
-arc_rad = 0.25
-nx.draw_networkx_edges(g.graph, pos, edgelist=curved_edges, edge_color=curved_edge_colors, connectionstyle=f'arc3, rad = {arc_rad}')
-edge_weights = nx.get_edge_attributes(g.graph, 'weigth')
-curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
-straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
-g.my_draw_networkx_edge_labels(g.graph, pos, edge_labels=curved_edge_labels,rotate=False,rad = arc_rad, plt=plt)
-nx.draw_networkx_edge_labels(g.graph, pos, edge_labels=straight_edge_labels,rotate=False)
-
-plt.show(block=False)
-input("a")
+    nx.draw_networkx_edges(g.graph, pos, edge_color=straight_edge_colors, edgelist=straight_edges)
+    arc_rad = 0.25
+    nx.draw_networkx_edges(g.graph, pos, edgelist=curved_edges, edge_color=curved_edge_colors, connectionstyle=f'arc3, rad = {arc_rad}')
+    edge_weights = nx.get_edge_attributes(g.graph, 'weigth')
+    curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
+    straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
+    g.my_draw_networkx_edge_labels(g.graph, pos, edge_labels=curved_edge_labels,rotate=False,rad = arc_rad, plt=plt)
+    plt.show(block=False)
+    plt.pause(3)
+    i = i + 1
