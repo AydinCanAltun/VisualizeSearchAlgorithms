2216eaa1c4078d1b336e67626aa54b9669e8e9f5
= <=>
2023-04-04 22:10:07 +0300

    Adım Adım BFS Algoritması

--- graph.py
+++ graph.py
@@ -29,7 +29,8 @@ class AlgorithmHelper:
         # Set of visited nodes to prevent loops
         visited = set()
         queue = Queue()
-
+        visited_edges = list()
+        self.show_graph(0, 0, visited_edges=visited_edges, pause=0)
         # Add the start_node to the queue and visited list
         queue.put(start_node)
         visited.add(start_node)
@@ -45,47 +46,14 @@ class AlgorithmHelper:
             if current_node == target_node:
                 path_found = True
                 break
-  
+            
             for (next_node, weight) in self.m_adj_list[current_node]:
                 if next_node not in visited:
-                    plt.clf()
-                    pos=nx.spring_layout(self.graph,seed=5)
-                    nx.draw_networkx_nodes(self.graph, pos)
-                    nx.draw_networkx_labels(self.graph, pos)
-
-                    curved_edges = [edge for edge in self.graph.edges() if reversed(edge) in self.graph.edges()]
-                    curved_edge_colors = []
-                    for (source, target) in curved_edges:
-                        if target in queue.queue:
-                            curved_edge_colors.append('red')
-                        elif target in visited:
-                            curved_edge_colors.append('orange')
-                        else:
-                            curved_edge_colors.append('black')
-                    straight_edges = list(set(self.graph.edges()) - set(curved_edges))
-                    straight_edge_colors = []
-                    for (source, target) in straight_edges:
-                        if target in queue.queue:
-                            straight_edge_colors.append('red')
-                        elif target in visited:
-                            straight_edge_colors.append('orange')
-                        else:
-                            straight_edge_colors.append('black')                
-                    print(f"straight_edge_colors: {straight_edge_colors}")
-                    nx.draw_networkx_edges(self.graph, pos, edge_color=straight_edge_colors, edgelist=straight_edges, arrowsize=50)
-                    arc_rad = 0.25
-                    nx.draw_networkx_edges(self.graph, pos, edgelist=curved_edges, edge_color=curved_edge_colors, connectionstyle=f'arc3, rad = {arc_rad}', arrowsize=50)
-                    edge_weights = nx.get_edge_attributes(self.graph, 'weigth')
-                    curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
-                    straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
-                    self.my_draw_networkx_edge_labels(self.graph, pos, edge_labels=curved_edge_labels,rotate=False,rad = arc_rad, plt=plt)
-                    nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=straight_edge_labels,rotate=False)
-                    plt.show(block=False)
-                    input()
+                    self.show_graph(current_node=current_node, next_node=next_node, visited_edges=visited_edges, pause=0)
                     queue.put(next_node)
                     parent[next_node] = current_node
                     visited.add(next_node)
-                    print(f"queue: {list(queue.queue)}")
+                    visited_edges.append((current_node, next_node))
                     
                 
         # Path reconstruction
@@ -183,6 +151,44 @@ class AlgorithmHelper:
         #         return True
         return None
     
+    def show_graph(self, current_node, next_node, visited_edges = list(), pause=10):
+        plt.clf()
+        pos=nx.spring_layout(self.graph,seed=5)
+        nx.draw_networkx_nodes(self.graph, pos)
+        nx.draw_networkx_labels(self.graph, pos)
+
+        curved_edges = [edge for edge in self.graph.edges() if reversed(edge) in self.graph.edges()]
+        curved_edge_colors = []
+        for (source, target) in curved_edges:
+            if source == current_node and target == next_node:
+                curved_edge_colors.append('red')
+            elif visited_edges.count((source, target)):
+                curved_edge_colors.append('orange')
+            else:
+                curved_edge_colors.append('black')
+        straight_edges = list(set(self.graph.edges()) - set(curved_edges))
+        straight_edge_colors = []
+        for (source, target) in straight_edges:
+            if source == current_node and target == next_node:
+                straight_edge_colors.append('red')
+            elif visited_edges.count((source, target)):
+                straight_edge_colors.append('orange')
+            else:
+                straight_edge_colors.append('black')
+        nx.draw_networkx_edges(self.graph, pos, edge_color=straight_edge_colors, edgelist=straight_edges, arrowsize=50)
+        arc_rad = 0.25
+        nx.draw_networkx_edges(self.graph, pos, edgelist=curved_edges, edge_color=curved_edge_colors, connectionstyle=f'arc3, rad = {arc_rad}', arrowsize=50)
+        edge_weights = nx.get_edge_attributes(self.graph, 'weigth')
+        curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
+        straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
+        self.my_draw_networkx_edge_labels(self.graph, pos, edge_labels=curved_edge_labels,rotate=False,rad = arc_rad, plt=plt)
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels=straight_edge_labels,rotate=False)
+        plt.show(block=False)
+        if pause > 0:
+            plt.pause(pause)
+        else:
+            input("")
+
     def my_draw_networkx_edge_labels(self,
         G,
         pos,
