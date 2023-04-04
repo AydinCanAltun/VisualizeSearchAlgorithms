50e0b10b1c2aff452050de69b4b74b24ddaf7a8a
= <=>
2023-04-04 17:40:52 +0300

    Edge, Ağırlıklarının eklenmesi sağlandı

--- main.py
+++ main.py
@@ -22,6 +22,11 @@ straight_edge_colors = ['red' if e[1] == 3 else 'black' for e in straight_edges]
 nx.draw_networkx_edges(g.graph, pos, edge_color=straight_edge_colors, edgelist=straight_edges)
 arc_rad = 0.25
 nx.draw_networkx_edges(g.graph, pos, edgelist=curved_edges, edge_color=curved_edge_colors, connectionstyle=f'arc3, rad = {arc_rad}')
-edge_weights = nx.get_edge_attributes(g.graph,'weight')
-nx.draw_networkx_edge_labels(g.graph, pos, edge_labels=edge_weights,rotate=False)
-plt.show()
+edge_weights = nx.get_edge_attributes(g.graph, 'weigth')
+curved_edge_labels = {edge: edge_weights[edge] for edge in curved_edges}
+straight_edge_labels = {edge: edge_weights[edge] for edge in straight_edges}
+g.my_draw_networkx_edge_labels(g.graph, pos, edge_labels=curved_edge_labels,rotate=False,rad = arc_rad, plt=plt)
+nx.draw_networkx_edge_labels(g.graph, pos, edge_labels=straight_edge_labels,rotate=False)
+
+plt.show(block=False)
+input("a")
